9f6dba5a8e3c8356902062ef3dd6f9e8ddeb0265
danielzmud1 <danielzmud1@gmial.com>
2015-08-14 09:14:42 +0200

    Uogolnienie funcji rysujacych - Tile

--- MonoGame2D/MonoGame2D/Game/Level.cs
+++ MonoGame2D/MonoGame2D/Game/Level.cs
@@ -27,15 +27,20 @@ namespace MonoGame2D.Game
         private int _baseScreenWidth;
         private int _baseScreenHeight;
 
-        private Tile[,] _tiles;
+        private Tile[,] _tiles1;
+        private Tile[,] _tiles2;
+        private int _tilesHeight;
+        private int _tilesWidth;
         private Texture2D[] _layers;
         private char _act;
         private int _level;
         
         
 
-        public int Width { get { return _tiles.GetLength(0); } }
-        public int Height { get { return _tiles.GetLength(1); } }
+        public int Width { get { return _tiles1.GetLength(0); } }
+        public int Height { get { return _tiles1.GetLength(1); } }
+
+        
 
         public Level(IServiceProvider serviceProvider, SpriteBatch spriteBatch, Vector2 baseScreenSize)
         {
@@ -49,15 +54,29 @@ namespace MonoGame2D.Game
         {
             _act = act;
             _level = level;
+            string levelPath;
+            Stream fileStream;
+            List<string> lines;
             // load textures
             // TO DO: check level and act must agree with this in level folder
-            string levelPath = string.Format("Content/Levels/{0}_level{1}.txt", act, level);
-            Stream fileStream = TitleContainer.OpenStream(levelPath) ;
-            LoadTiles(fileStream);
+
+            // first render, background
+            levelPath = string.Format("Content/Levels/{0}{1}_level_1.txt", act, level);
+            fileStream = TitleContainer.OpenStream(levelPath) ;
+            lines = LoadTileFromTxt(fileStream);
+            _tiles1 = new Tile[_tilesWidth, _tilesHeight];
+            LoadTile(lines, _tiles1, new DelegateLoadTile(LoadTile1));
+
+            // secound render
+            levelPath = string.Format("Content/Levels/{0}{1}_level_2.txt", act, level);
+            fileStream = TitleContainer.OpenStream(levelPath);
+            lines = LoadTileFromTxt(fileStream);
+            _tiles2 = new Tile[_tilesWidth, _tilesHeight];
+            LoadTile(lines, _tiles2, new DelegateLoadTile(LoadTile2));
         
         }
 
-        private void LoadTiles(Stream fileStream)
+        private List<string> LoadTileFromTxt(Stream fileStream)
         {
             int width;
             List<string> lines = new List<string>();
@@ -73,51 +92,83 @@ namespace MonoGame2D.Game
                     line = reader.ReadLine();
                 }
 
-                _tiles = new Tile[width, lines.Count];
-                for (int y = 0; y < Height; ++y)
-                {
-                    for (int x = 0; x < Width; ++x)
-                    {
-                        char tileType = lines[y][x];
-                        _tiles[x, y] = LoadTile(tileType, x, y);
-                    }
-                }
+                _tilesWidth = width;
+                _tilesHeight = lines.Count;
 
-            
-            
+                return lines;
             }
         }
 
-        private Tile LoadTile(char tileType, int x, int y)
+
+
+        private void LoadTile(List<string> lines, Tile[,] tiles, DelegateLoadTile LoadTile)
+        {
+            for (int y = 0; y < _tilesHeight; ++y)
+                for (int x = 0; x < _tilesWidth; ++x)
+                {
+                    char tileType = lines[y][x];
+                    tiles[x, y] = LoadTile(tileType, x, y);
+                } 
+        }
+
+        private delegate Tile DelegateLoadTile(char Type, int x, int y);
+
+        private Tile LoadTile1(char tileType, int x, int y)
         {
             switch (tileType)
             {
+                case '*':
+                    return new Tile(null, TileCollision.cos);
                 case '.':
                     return LoadTile("Block1", TileCollision.Tile);
-
                 default:
                     throw new NotSupportedException(String.Format("Not Parrsed character in level.txt"));
             }
         }
+
+        private Tile LoadTile2(char tileType, int x, int y)
+        {
+            switch (tileType)
+            {
+                case '*':
+                    return new Tile(null, TileCollision.cos);
+                case '1':
+                    return LoadTile("Rock1", TileCollision.Tile);
+                case '2':
+                    return LoadTile("Rock2", TileCollision.Tile);
+                case '3':
+                    return LoadTile("Busches1", TileCollision.Tile);
+                case '4':
+                    return LoadTile("Wood1", TileCollision.Tile);
+                default:
+                    throw new NotSupportedException(String.Format("Not Parrsed character in level.txt"));
+            }
+        }
+
+        
 
         private Tile LoadTile(string name, TileCollision collision)
         {
             return new Tile(Content.Load<Texture2D>("Tiles/" + _act + "_" + name), collision);
         }
 
+
+
+
         public void Draw()
         {
-            DrawTiles();
+            DrawTiles(_tiles1);
+            DrawTiles(_tiles2);
         
         }
 
-        private void DrawTiles()
+        private void DrawTiles(Tile[,] tiles)
         {
-            for (int y = 0; y < Height; ++y)
+            for (int y = 0; y < _tilesHeight; ++y)
             {
-                for (int x = 0; x < Width; ++x)
+                for (int x = 0; x < _tilesWidth; ++x)
                 {
-                    Texture2D texture = _tiles[x, y].Texture;
+                    Texture2D texture = tiles[x, y].Texture;
                     if (texture != null)
                     {
                         Vector2 position = new Vector2(x, y) * Tile.Size;
@@ -125,7 +176,6 @@ namespace MonoGame2D.Game
                     }
                 }
             }
-
         }
 
 
